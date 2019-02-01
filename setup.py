6b436b6e75a0814039391c764715aaf8e4058a6b
Matt Broach <go.for.dover@gmail.com>
2019-02-01 00:59:55 -0500

    fixing missing pypi README

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ from setuptools import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
 setup(
     name='django-merged-inlines',
-    version='1.0.2',
+    version='1.0.3',
     packages=['merged_inlines'],
     include_package_data=True,
-    license='MIT License',
+    license='MIT',
     description='A Django Admin extension that allows you to mix and reorder multiple inline classes together',
     url='https://github.com/MattBroach/Django-Merged-Inlines',
+    long_description=README,
     author='Matt Broach, Koty Yell',
     author_email='broach@aya.yale.edu',
     classifiers=[
